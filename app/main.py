47f2e246aac67bcdb1f5b2aa23b5d12f2d6cd349
Myroslav Vorobei <miroslavcomputer@gmail.com>
2025-03-21 11:22:32 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,36 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str, health: int = 100, hidden: bool = False) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+
+    def update_health(self, amount: int) -> None:
+        self.health += amount
+        if self.health <= 0:
+            print(f"{self.name} is died!")
+            Animal.alive.remove(self)
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        if self.hidden:
+            self.hidden = False
+        else:
+            self.hidden = True
+
+
+class Carnivore(Animal):
+    def bite(self, prey: Herbivore) -> None:
+        if not isinstance(prey, Herbivore):
+            print(f"{prey.name} is not a Herbivore!")
+        elif prey.hidden:
+            print(f"The prey {prey.name} is hidden!")
+        else:
+            print(f"The prey {prey.name} is bitten!")
+            prey.update_health(-50)
